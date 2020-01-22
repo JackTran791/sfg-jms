27c12a85397efc426dc1df3a6922e618e779a3ec
JackTran791 <congsonit@gmail.com>
2020-01-22 13:32:46 -0600

    - Adding Task Configuration.

---
+++ src/main/java/com/luv2code/sfgjms/config/TaskConfig.java
@@ -0,0 +1,22 @@
+package com.luv2code.sfgjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author Jack Tran
+ */
+@EnableScheduling
+@EnableAsync
+@Configuration
+public class TaskConfig {
+
+    @Bean
+    TaskExecutor taskExecutor() {
+        return new SimpleAsyncTaskExecutor();
+    }
+}
